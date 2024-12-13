602912396bc8d45cefd66fdb202e615147b3c0d0
Benjamin.Maveau <benjamin.maveau@gmail.com>
2024-12-13 22:23:32 +0100

    Solution for day 11 part 2

--- 2024/11.py
+++ 2024/11.py
@@ -24,6 +24,40 @@ def blink(data):
 
 
 ndata = data
-for i in range(75):
+for i in range(25):
     ndata = blink(ndata)
     print(i, len(ndata))
+
+
+# Part 2
+
+
+def blink_one(number, n):
+    if (number, n) in blink_one._cached:
+        return blink_one._cached[(number, n)]
+    res: int = 0
+    if n == 1:
+        if number == "0":
+            res = 1
+        elif len(number) % 2 == 0:
+            res = 2
+        else:
+            res = 1
+    elif number == "0":
+        res = blink_one("1", n - 1)
+    elif len(number) % 2 == 0:
+        idx = len(number) // 2
+        a = number[:idx]
+        b = number[idx:].lstrip("0")
+        if b == "":
+            b = "0"
+        res = blink_one(a, n - 1) + blink_one(b, n - 1)
+    else:
+        res = blink_one(str(int(number) * 2024), n - 1)
+    blink_one._cached[(number, n)] = res
+    return res
+
+
+blink_one._cached = {}
+
+print(sum(blink_one(d, 75) for d in data))
