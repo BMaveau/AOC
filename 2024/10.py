62ec77830405dc1c4977b1190e16f1cca517f6ea
Benjamin.Maveau <benjamin.maveau@gmail.com>
2024-12-13 21:28:31 +0100

    Solution for day 10 part 2

--- 2024/10.py
+++ 2024/10.py
@@ -10,7 +10,7 @@ test = """89010123
 01329801
 10456732"""
 
-data: DataMatrix = aoc.load_data(False, test, 10, is_2d=True)
+data: DataMatrix = aoc.load_data(True, test, 10, is_2d=True)
 
 start = aoc.Pos(0, 0)
 
@@ -25,6 +25,25 @@ for th in data.find_iter("0", start):
             for d in pdir:
                 if (res := p + d) in data and data[res] == i:
                     n_pos.add(res)
+        pos = n_pos
+    cnt += len(n_pos)
+
+print(cnt)
+
+
+# Part 2
+
+cnt = 0
+pdir = [aoc.Dir(-1, 0), aoc.Dir(1, 0), aoc.Dir(0, -1), aoc.Dir(0, 1)]
+for th in data.find_iter("0", start):
+    print(th)
+    pos = [th]
+    for i in "123456789":
+        n_pos = []
+        for p in pos:
+            for d in pdir:
+                if (res := p + d) in data and data[res] == i:
+                    n_pos.append(res)
         pos = n_pos
     cnt += len(n_pos)
 
