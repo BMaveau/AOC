99f95392b7a27d4de710d1990b1601e9f00fe64c
BMaveau <benjamin.maveau@gmail.com>
2025-12-06 20:13:07 +0100

    Cache results for speedup

--- 2025/day3.py
+++ 2025/day3.py
@@ -1,11 +1,13 @@
 from itertools import tee
 
 from aoc.data import load_data
+from aoc.helpers import cache_results
 
 def find_pos(l, el):
     return [i for i, x in enumerate(l) if x == el]
 
-def find_largest_joltage(bank: list[int], n):
+@cache_results
+def find_largest_joltage(bank: tuple[int, ...], n):
     if len(bank) == n:
         return int(''.join(str(i) for i in bank))
     if n == 1:
@@ -25,7 +27,7 @@ test_inp = """987654321111111
 818181911112111"""
 
 inp = load_data(False, test_inp, 3, True)
-inp = map(lambda bank: [int(b) for b in bank], inp)
+inp = map(lambda bank: tuple(int(b) for b in bank), inp)
 
 inp_a, inp_b = tee(inp)
 print(sum(map(lambda i: find_largest_joltage(i, 2), inp_a)))
